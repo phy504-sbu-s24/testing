849239fb9dad4d705323f819351a963c70ce4fcc
Michael Zingale <michael.zingale@stonybrook.edu>
2024-03-24 14:26:11 -0400

    add test

---
+++ testing.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cassert>
+#include <vector>
+
+int main() {
+
+    std::vector<double> a(10, 0.0);
+
+    for (int n = 0; n < a.size(); ++n) {
+        a[n] = static_cast<double>(n);
+    }
+
+    assert(a[2] == 2.0);
+}
